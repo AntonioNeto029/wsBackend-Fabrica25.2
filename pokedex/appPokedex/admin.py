08559835bd360a2ba50ce801ab000b3de99773ff
AntonioNeto1203 <antonioramos9@hotmail.com>
2025-09-14 05:00:12 -0300

    refactor: Ajustes nos arquivos models.py,  migratios.py e  admin.py

--- pokedex/appPokedex/admin.py
+++ pokedex/appPokedex/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Pokemon
+from .models import Pokemon, Treinador
 
 admin.site.register(Pokemon)
+admin.site.register(Treinador)
 
 # Register your models here.
--- pokedex/appPokedex/models.py
+++ pokedex/appPokedex/models.py
@@ -2,15 +2,18 @@ from django.db import models
 
 class Pokemon(models.Model):
     nome = models.CharField(max_length=100)
-    tipo = models.CharField()
+    tipo = models.CharField(max_length=100)
     peso = models.IntegerField()
     altura = models.IntegerField()
     imagemUrl = models.URLField(blank=True, null=True)
+    
+    def __str__(self):
+        return self.nome
     
 class Treinador(models.Model):
     nome = models.CharField(max_length=100)
     idade = models.IntegerField()
       
-def __str__(self):
-    return self.name
+    def __str__(self):
+        return self.nome
 
